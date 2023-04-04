e8fb200469eadb0801dffd14c3a2f6592b201c43
jordislpm <jordislpm@gmail.com>
2023-04-04 15:22:45 -0400

    [feat] poke api running

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import PokemonApp from './components/PokemonApp'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PokemonApp/>
   </React.StrictMode>,
 )
