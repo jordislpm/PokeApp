503c966e89d78d9f6ea9859c95dadc310cb99532
jordislpm <jordislpm@gmail.com>
2023-04-04 15:09:40 -0400

    [feat] aplied PokemonApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import './App.css'
-
+import PokemonApp from './components/PokemonApp'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-     poke app
-    </div>
+    <>
+     <PokemonApp/>
+    </>
   )
 }
 
