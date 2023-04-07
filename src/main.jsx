63f40a139f6fd9436f0fd61dff4c7b8e655729b5
jordislpm <jordislpm@gmail.com>
2023-04-07 12:30:53 -0400

    [feat] update styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import PokemonApp from './components/PokemonApp'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
